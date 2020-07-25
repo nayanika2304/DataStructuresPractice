6c773f25eb67b293f2fe1ab22996ce91b7a432a3
nayanikabhargava <nayanikabhargava1993@gmail.com>
2020-07-25 15:44:31 -0400

    Initial commit

---
+++ Simple Linked List/delete_a node.py
@@ -0,0 +1,66 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+
+class SLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def Atbegining(self, data_in):
+        NewNode = Node(data_in)
+        NewNode.next = self.head
+        self.head = NewNode
+
+    '''
+    take the key of the node to be deleted
+    if the key is head self.head = headval.next
+    headval = None
+    
+    else iterate through the list with headval
+    if headval.data = key break
+    else maintain a record of the prev node 
+    and keep iterating on headval = headval.next
+    
+    to delete make prev.next = Headval.next 
+    headval = none 
+    '''
+    # Function to remove node
+    def RemoveNode(self, Removekey):
+
+        HeadVal = self.head
+
+        if (HeadVal is not None):
+            if (HeadVal.data == Removekey):
+                self.head = HeadVal.next
+                HeadVal = None
+                return
+
+        while (HeadVal is not None):
+            if HeadVal.data == Removekey:
+                break
+            prev = HeadVal
+            HeadVal = HeadVal.next
+
+        if (HeadVal == None):
+            return
+
+        prev.next = HeadVal.next
+
+        HeadVal = None
+
+    def LListprint(self):
+        printval = self.head
+        while (printval):
+            print(printval.data),
+            printval = printval.next
+
+
+llist = SLinkedList()
+llist.Atbegining("Mon")
+llist.Atbegining("Tue")
+llist.Atbegining("Wed")
+llist.Atbegining("Thu")
+llist.RemoveNode("Tue")
+llist.LListprint()
